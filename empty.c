32b2f95e6ad2c1fccdbbd14988ddbce8ba5ce9c0
Radosław Ziomber <rziomber@op.pl>
2024-05-05 18:59:08 +0200

    Update empty.c

--- empty.c
+++ empty.c
@@ -9,7 +9,7 @@ static void activate (GtkApplication* app,
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-  gtk_widget_show (window);
+  gtk_window_present(GTK_WINDOW(window));
 }
 
 int main (int argc, char **argv)
